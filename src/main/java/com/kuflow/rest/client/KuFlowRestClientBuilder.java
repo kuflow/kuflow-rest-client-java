d3d2357cfa4501da1f87fa52d554a50d2596381f
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2022-11-18 15:06:58 +0000

    [KF-391] License

--- src/main/java/com/kuflow/rest/client/KuFlowRestClientBuilder.java
+++ src/main/java/com/kuflow/rest/client/KuFlowRestClientBuilder.java
@@ -1,3 +1,25 @@
+/*
+ * The MIT License
+ * Copyright © 2022-present KuFlow S.L.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.kuflow.rest.client;
 
 import static com.azure.core.util.AuthorizationChallengeHandler.WWW_AUTHENTICATE;
@@ -294,7 +316,7 @@ public final class KuFlowRestClientBuilder
      * and {@link #retryPolicy(RetryPolicy)} have been set.
      */
     public KuFlowRestClient buildClient() {
-        return new KuFlowRestClient(createServiceImpl());
+        return new KuFlowRestClient(this.createServiceImpl());
     }
 
     private KuFlowClientImpl createServiceImpl() {
@@ -359,7 +381,7 @@ public final class KuFlowRestClientBuilder
 
     private HttpPipelinePolicy createHttpPipelineAuthPolicy() {
         if (this.username == null || this.password == null) {
-            throw logger.logExceptionAsError(new IllegalArgumentException("Both 'username' and 'username' are required."));
+            throw this.logger.logExceptionAsError(new IllegalArgumentException("Both 'username' and 'username' are required."));
         }
 
         BasicAuthenticationCredential tokenCredential = new BasicAuthenticationCredential(this.username, this.password);
